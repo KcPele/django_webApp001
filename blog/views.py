64db44fb7e1c2ab2ffbdcbd36dfbf882aa76c67e
kc emmanuel <fidekg123@gmail.com>
2021-08-09 15:05:10 +0100

    added styles

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+
+posts = [
+   {
+   		'author':'Kcpele',
+   		'title':'Blog post 1',
+   		'content':'First post content',
+   		'date_posted':'August 78'
+   },
+   {
+   		'author':'Gift',
+   		'title':'Blog post 2',
+   		'content':'second post content',
+   		'date_posted':'August 78'
+   }
+]
 
 def home(request):
-	return render(request, 'blog/home.html')
+	context = {
+	'posts':posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('Blog About')
+	return render(request, 'blog/about.html', {'title':'About'})
